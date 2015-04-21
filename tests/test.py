f8b3cee946e13d13b4d2793f3a40fefeeabb3419
Jenny Duckett <jenny.duckett@digital.cabinet-office.gov.uk>
2015-04-21 15:05:12 +0100

    Write full aggregated data to a CSV file before posting to PP
    The filename includes the start and end dates, eg
    `report_2015-03-11_2015-04-22.csv`.
    
    TemporaryDirectory comes from http://stackoverflow.com/a/22726782.

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,22 @@
 # coding=utf-8
 
+from contextlib import contextmanager
 from datetime import date, datetime
 import json
 import logging
 import os
 import re
+from tempfile import mkdtemp
+import shutil
 import unittest
 import urllib
 
+from mock import patch, mock_open
 import responses
 
 from stats.info_statistics import (
     AggregatedDataset,
+    CSVWriter,
     Datapoint,
     GOVUK,
     InfoStatistics,
@@ -201,6 +206,53 @@ class TestPerformancePlatform(unittest.TestCase):
                          expected_pageview_counts)
 
 
+@contextmanager
+def TemporaryDirectory():
+    name = mkdtemp()
+    try:
+        yield name
+    finally:
+        shutil.rmtree(name)
+
+
+class TestCSVWriter(unittest.TestCase):
+    def test_default_filename(self):
+        start_date = datetime(2015, 2, 3, 9, 15, 30)
+        end_date = datetime(2015, 3, 6, 14, 27, 17)
+        writer = CSVWriter(start_date=start_date, end_date=end_date)
+
+        expected_filename = 'report_2015-02-03_2015-03-06.csv'
+        self.assertEqual(writer.output_filename, expected_filename)
+
+    def test_writing_csv(self):
+        datapoints = [
+            self.build_datapoint_with_counts('/path1'),
+            self.build_datapoint_with_counts('/path2'),
+        ]
+
+        expected_csv_lines = [
+            'uniquePageviews,problemReports,searchUniques,pagePath,_id,problemsPer100kViews,searchesPer100kViews',
+            '10,2,5,/path1,_path1,20000.0,50000.0',
+            '10,2,5,/path2,_path2,20000.0,50000.0',
+        ]
+
+        with TemporaryDirectory() as tempdir:
+            csv_filename = os.path.join(tempdir, 'test_report.csv')
+            writer = CSVWriter(output_filename=csv_filename)
+            writer.write_datapoints(datapoints)
+
+            with open(csv_filename, 'r') as open_file:
+                file_lines = open_file.read().splitlines()
+                self.assertEqual(file_lines, expected_csv_lines)
+
+    def build_datapoint_with_counts(self, path):
+        datapoint = Datapoint(path)
+        datapoint.set_problem_reports_count(2)
+        datapoint.set_search_count(5)
+        datapoint.set_pageview_count(10)
+        return datapoint
+
+
 class TestAggregatedDataset(unittest.TestCase):
     def test_aggregated_dataset(self):
         aggregate = AggregatedDataset()
@@ -255,6 +307,7 @@ class TestInfoStatistics(unittest.TestCase):
                                    end_date=date(2015, 01, 27))
 
     @responses.activate
+    @patch('__builtin__.open', new=mock_open())
     def test_data_processing(self):
         searches = """
         {
